36420986bdf7cc2e2830069a5997e280bcbd3899
Detroid Wild <shalker760@gmail.com>
2025-10-19 12:33:22 +0300

    роо

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
             
       {/* Основной контент */}
       <div className="max-w-4xl text-center relative z-10">
-        <h1 className="text-5xl md:text-7xl font-extrabold text-transparent bg-clip-text 
+        <h1 className="text-5xl md:text-7xl font-extrabold bg-clip-text 
                       bg-gradient-to-r mb-8">
           <TypingText text="Добро пожаловать жители Кроймена" />
         </h1>
